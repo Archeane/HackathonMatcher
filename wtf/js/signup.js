d98710fab2db3dc18b12003ef31fc004b96dd1b5
archeane <jenny.xu@stonybrook.edu>
2018-07-18 11:10:30 -0400

    hackathon selector done

--- wtf/js/signup.js
+++ wtf/js/signup.js
@@ -128,11 +128,12 @@ $(document).ready(function () {
 	});
 
 	//------------hackathon select-------------------
-	 $('#hackathons').change(function () {
-	 	if ($('#hackathons').val() != 0) {
+	var attendedHackathons = [];
+	 $('#reg-hackathons').change(function () {
+	 	if ($('#reg-hackathons').val() != 0) {
 	 		var container = document.querySelector('#selectHackathons');
 	 		container.innerHTML = '';
-	 		if ($('#hackathons').val() > 5) {
+	 		if ($('#reg-hackathons').val() > 5) {
 	 			container.innerHTML += '<div class="row">';
 	 			container.innerHTML += '<label>Choose Your Top 5 Hackathons</label>';
 	 			container.innerHTML += '</div>';
@@ -141,11 +142,11 @@ $(document).ready(function () {
 	 			container.innerHTML += '<label>Which Hackathons?</label>';
 	 			container.innerHTML += '</div>';
 	 		}
-	 		for (i = 1; i <= Math.min($('#hackathons').val(), 5); i++) {
+	 		for (i = 1; i <= Math.min($('#reg-hackathons').val(), 5); i++) {
 	 			var content = '';
 
 	 			content += '<div class="row">';
-	 			content += '<select placeholder="Year" id="hack' + i + '">';
+	 			content += '<select placeholder="Year" class="hackYear" id="hack' + i + '">';
 	 			content += '<option value="2014">2014</option>';
 	 			content += '<option value="2015">2015</option>';
 	 			content += '<option value="2016">2016</option>';
@@ -155,37 +156,34 @@ $(document).ready(function () {
 	 			content += '</div>';
 	 			container.innerHTML += content;
 	 		}
-			for(i = 1; i <= Math.min($('#hackathons').val(), 5); i++){
-				var content = '';
-	 			$('#hack' + i).on('change', function() {
-					//TODO: fix querySelector is not a function
-					//var ctncontainer = $(this).querySelector('.selectHack');
-	 				var content = '<select name="hackathon" placeHolder="hackathon" style="width:70%;">';
-					//ctncontainer.innerHTML = '';
-	 				var year = $(this).val();
 
-	 				$.getJSON({
-						method: "GET",
-	 					url: '../../wtf/assets/hackathons' + year + '.json', // wherever your data is actually coming from
-						data: {name:'value'},
-						dataType: 'JSON'
-	 				}).done(function(data){
-						for (var d = 0; d < data.length; d++) {
-	 						var item = data[d];
-							var option = '<option value="'+item.name+'">'+item.name+'</option>';
-	 						//var option = new Option(item.name, item.id, false, false);
-	 						content += option;
-	 					}
-	 					//$hackathon.trigger('change');
-						content += '</select>';
-						container.innerHTML += content;
-						console.log(container.innerHTML);
-					}).fail(function(data)  {
-						alert("Sorry. Server unavailable. ");
-					});
+			$('.hackYear').on('change', function(){
+				console.log($(this).attr('id'));
+				var selectHack = $(this).parent().children().last();
+				selectHack.append('<select name="hackathon" style="width:100%; margin-left:20px;">');
+				var select = selectHack.children();
+				var year = $(this).val();
+				console.log(year);
+				$.getJSON({
+					method: "GET",
+					url: '../../wtf/assets/hackathons' + year + '.json', // wherever your data is actually coming from
+					data: {name:'value'},
+					dataType: 'JSON'
+				}).done(function(data){
+					for (var d = 0; d < data.length; d++) {
+						var item = data[d];
+						//var option = '<option value="'+item.name+'">'+item.name+'</option>';
+						var option = new Option(item.name, item.id, false, false);
+						select.append(option);
+						//content += option;
+					}
+					//$hackathon.trigger('change');
+					//selectHack.append('</select>');
+				}).fail(function(data)  {
+					alert("Sorry. Server unavailable. ");
+				});
 
-	 			});
-			}
+			});
 	 	}
 	 });
 });
@@ -205,69 +203,67 @@ $('#msform').on('keyup keypress', function (e) {
 	}
 });
 
-
-
 //user information 
 var user = {};
 
 $(".next").click(function () {
 	//validate fields are set
 	//console.log(($('#reg-school').val() && $('#reg-major').val() && $('#reg-graduationYear').val() && $('#reg-educationLevel').val()));
-	//if(!($('#reg-school').val() && $('#reg-major').val() && $('#reg-graduationYear').val() && $('#reg-educationLevel').val())){
-	$('#reg-error').text('Please fill in all required fields');
-	$('#reg-error').show();
-	//}else{
-	$('#reg-error').hide();
+	if (!($('#reg-school').val() && $('#reg-major').val() && $('#reg-graduationYear').val() && $('#reg-educationLevel').val())) {
+		$('#reg-error').text('Please fill in all required fields');
+		$('#reg-error').show();
+	} else {
+		$('#reg-error').hide();
 
-	//append data to user variable
-	user['school'] = $('#reg-school');
-	user['major'] = $('#reg-major');
-	user['graduationYear'] = $('#reg-graduationYear');
-	user['educationLevel'] = $('#reg-educationLevel');
+		//append data to user variable
+		user['school'] = $('#reg-school');
+		user['major'] = $('#reg-major');
+		user['graduationYear'] = $('#reg-graduationYear');
+		user['educationLevel'] = $('#reg-educationLevel');
+		user['hackathons'] = $('#reg-hackathons');
 
-	//change form animation
+		//change form animation
+		if (animating) return false;
+		animating = true;
 
-	if (animating) return false;
-	animating = true;
+		current_fs = $(this).parent();
+		next_fs = $(this).parent().next();
 
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
+		//activate next step on progressbar using the index of next_fs
+		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-	//show the next fieldset
-	next_fs.show();
-	//hide the current fieldset with style
-	current_fs.animate({
-		opacity: 0
-	}, {
-		step: function (now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50) + "%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({
-				'transform': 'scale(' + scale + ')',
-				'position': 'absolute'
-			});
-			next_fs.css({
-				'left': left,
-				'opacity': opacity
-			});
-		},
-		duration: 800,
-		complete: function () {
-			current_fs.hide();
-			animating = false;
-		},
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-	//}
+		//show the next fieldset
+		next_fs.show();
+		//hide the current fieldset with style
+		current_fs.animate({
+			opacity: 0
+		}, {
+			step: function (now, mx) {
+				//as the opacity of current_fs reduces to 0 - stored in "now"
+				//1. scale current_fs down to 80%
+				scale = 1 - (1 - now) * 0.2;
+				//2. bring next_fs from the right(50%)
+				left = (now * 50) + "%";
+				//3. increase opacity of next_fs to 1 as it moves in
+				opacity = 1 - now;
+				current_fs.css({
+					'transform': 'scale(' + scale + ')',
+					'position': 'absolute'
+				});
+				next_fs.css({
+					'left': left,
+					'opacity': opacity
+				});
+			},
+			duration: 800,
+			complete: function () {
+				current_fs.hide();
+				animating = false;
+			},
+			//this comes from the custom easing plugin
+			easing: 'easeInOutBack'
+		});
+	}
 });
 
 $(".previous").click(function () {
