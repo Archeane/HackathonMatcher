95020f21e784a6f4c009b6ce33017869e759061d
archeane <jenny.xu@stonybrook.edu>
2018-07-31 14:35:43 -0400

    filters linked with visualization!

--- wtf/js/visualization.js
+++ wtf/js/visualization.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
 	for(i = 0; i < allMatches.length; i++){
 		matches.push(JSON.parse(allMatches[i]));
 	}
-	display(matches);
-/*
+	
 
 	var allMajors = [];
 	var allGradYears = [];
@@ -28,8 +27,10 @@ $(document).ready(function(){
 		}
 	}
 
-	var gradFilter = document.querySelector(".gradYear");
+	var gradFilter = document.querySelector("#graduationYear > .dropdown-menu");
 	for(g = 0; g < allGradYears.length; g++){
+		var div = document.createElement('div');
+		div.class="dropdown-item";
 		var checkBox = document.createElement('input');
 		checkBox.type="checkbox";
 		checkBox.name=allGradYears[g];
@@ -39,11 +40,14 @@ $(document).ready(function(){
 		var label = document.createElement('label');
 		label.htmlFor = allGradYears[g];
 		label.appendChild(document.createTextNode(allGradYears[g]));
-		gradFilter.appendChild(checkBox);
-		gradFilter.appendChild(label);
+		div.appendChild(checkBox);
+		div.appendChild(label);
+		gradFilter.appendChild(div);
 	}
-	var eduFilter = document.querySelector(".eduLevel");
+	var eduFilter = document.querySelector("#educationLevel > .dropdown-menu");
 	for(m = 0; m < allEduLevels.length; m++){
+		var div = document.createElement('div');
+		div.class="dropdown-item";
 		var checkBox = document.createElement('input');
 		checkBox.type="checkbox";
 		checkBox.name=allEduLevels[m];
@@ -53,11 +57,14 @@ $(document).ready(function(){
 		var label = document.createElement('label');
 		label.htmlFor = allEduLevels[m];
 		label.appendChild(document.createTextNode(allEduLevels[m]));
-		eduFilter.appendChild(checkBox);
-		eduFilter.appendChild(label);
+		div.appendChild(checkBox);
+		div.appendChild(label);
+		eduFilter.appendChild(div);
 	}
-	var hacksFilter = document.querySelector(".numOfHacks");
+	var hacksFilter = document.querySelector("#hackathons > .dropdown-menu");
 	for(m = 0; m < allNumOfHacks.length; m++){
+		var div = document.createElement('div');
+		div.class="dropdown-item";
 		var checkBox = document.createElement('input');
 		checkBox.type="checkbox";
 		checkBox.name=allNumOfHacks[m];
@@ -67,11 +74,14 @@ $(document).ready(function(){
 		var label = document.createElement('label');
 		label.htmlFor = allNumOfHacks[m];
 		label.appendChild(document.createTextNode(allNumOfHacks[m]));
-		hacksFilter.appendChild(checkBox);
-		hacksFilter.appendChild(label);
+		div.appendChild(checkBox);
+		div.appendChild(label);
+		hacksFilter.appendChild(div);
 	}
-	var majorsFilter = document.querySelector(".major");
+	var majorsFilter = document.querySelector("#major > .dropdown-menu");
 	for(m = 0; m < allMajors.length; m++){
+		var div = document.createElement('div');
+		div.class="dropdown-item";
 		var checkBox = document.createElement('input');
 		checkBox.type="checkbox";
 		checkBox.name=allMajors[m];
@@ -81,12 +91,11 @@ $(document).ready(function(){
 		var label = document.createElement('label');
 		label.htmlFor = allMajors[m];
 		label.appendChild(document.createTextNode(allMajors[m]));
-		majorsFilter.appendChild(checkBox);
-		majorsFilter.appendChild(label);
+		div.appendChild(checkBox);
+		div.appendChild(label);
+		majorsFilter.appendChild(div);
 	}
-
-
-   
+/*
 	var j = JSON.stringify({'content':'hello','csrfmiddlewaretoken': '{{ csrf_token }}'})
 	var filters = [];
 	$('#submitFilters').on('click', (event) => {
@@ -99,41 +108,63 @@ $(document).ready(function(){
 		var filtersJSON = JSON.stringify(filters)
 		//TODO: replace hardcoded url
 		$.ajax(
-		    {
-		        type: 'POST',
-		        url: 'http://localhost:8080/test',
-		        data: filtersJSON,
-		        success: function(data){
-			       console.log(data);
-			    },
-	            error: function(jqXHR, textStatus, err) {
-	                //show error message
-	              	console.log('text status '+textStatus+', err '+err)
-	            }
-		    }
+			{
+				type: 'POST',
+				url: 'http://localhost:8080/test',
+				data: filtersJSON,
+				success: function(data){
+				   console.log(data);
+				},
+				error: function(jqXHR, textStatus, err) {
+					//show error message
+					console.log('text status '+textStatus+', err '+err)
+				}
+			}
 		);
 	});
-	
-	/*
+*/	
+	var filterString = '{"major": [],"graduationYear": [],"educationLevel": [],"hackathons": []}';
+	var filters = JSON.parse(filterString);
+	//var filters = [['majors',[]],['gradYear',[]],['eduLevel',[]],['numOfHacks',[]]];
 	$(':checkbox').on('change',function(){
 		if($(this).prop('checked')){
-			filters.push($(this).val());
-			console.log(filters);
+			//filters.push($(this).val());
+			$('#vis').empty();
+			var parent = $(this).parentsUntil(".dropdown").parent().attr('id');
+			filters[parent].push($(this).val());
+			console.log(filters[parent]);
+			display(matches, filters);
 		}else{
-			var index = filters.indexOf($(this).val());
-			if (index !== -1) filters.splice(index, 1);
-			console.log(filters);
+			$('#vis').empty();
+			display(matches);
 		}
 	});
-	*/
+	display(matches);
 });
 
 
 function bubbleChart() {
   var width = 1200;
-  var height = 800;
+  var height = 1200;
+
+  var tooltip = floatingTooltip('hackers_tooltip', 240);
 
   var center = { x: width / 2, y: height / 2 };
+  const threeClusterCenters = {
+  	1: { x: width / 4, y: height / 2 },
+    2: { x: width / 2, y: height / 2 },
+    3: { x: 2 * width / 3, y: height / 2 }
+  };
+  const sixClusterCenters = {
+  	1: { x: width / 4, y: height / 4 },
+    2: { x: width / 2, y: height / 4 },
+    3: { x: 2 * width / 3, y: height / 4 },
+    4: { x: width / 4, y: 3*height / 4 },
+    5: { x: width / 2, y: 3*height / 4 },
+    6: { x: 2 * width / 3, y: 3*height / 4 }
+  };
+
+  
   var damper = 0.102;
 
   // These will be set in create_nodes and create_vis
@@ -142,180 +173,186 @@ function bubbleChart() {
   var nodes = [];
 
   function charge(d) {
-    return -Math.pow(d.radius, 2.0) / 8;
+	return -Math.pow(d.radius, 2.0) / 8;
   }
 
   var force = d3.layout.force()
-    .size([width, height])
-    .charge(charge)
-    .gravity(-0.01)
-    .friction(0.9);
+	.size([width, height])
+	.charge(charge)
+	.gravity(-0.01)
+	.friction(0.9);
 
 
   var fillColor = d3.scale.ordinal()
-    .domain(['undergraduate', 'graduate', 'PhD'])
-    .range(['#d84b2a', '#beccae', '#7aa25c']);
+	.domain(['undergraduate', 'graduate', 'PhD'])
+	.range(['#d84b2a', '#beccae', '#7aa25c']);
 
   var radiusScale = d3.scale.pow()
-    .exponent(0.5)
-    .range([2, 85]);
+	.exponent(0.5)
+	.range([2, 85]);
 
-  /*
-   * This data manipulation function takes the raw data from
-   * the CSV file and converts it into an array of node objects.
-   * Each node will store data and visualization values to visualize
-   * a bubble.
-   *
-   * rawData is expected to be an array of data objects, read in from
-   * one of d3's loading functions like d3.csv.
-   *
-   * This function returns the new node array, with a node in that
-   * array for each element in the rawData input.
-   */
   function createNodes(rawData) {
-    // Use map() to convert raw data into node data.
-    // Checkout http://learnjsdata.com/ for more on
-    // working with data.
-    var myNodes = rawData.map(function (d) {
-      return {
-        radius: radiusScale(+d.score),
-        value: d.score,
-        name: d.name,
-        school: d.school,
-        major: d.major,
-        graduationYear: d.graduationYear,
-        educationLevel: d.educationLevel,
-        x: Math.random() * 900,
-        y: Math.random() * 800
-      };
-    });
+	// Use map() to convert raw data into node data.
+	// Checkout http://learnjsdata.com/ for more on
+	// working with data.
+	var myNodes = rawData.map(function (d) {
+	  return {
+		radius: radiusScale(+d.score),
+		value: d.score,
+		name: d.name,
+		school: d.school,
+		major: d.major,
+		graduationYear: d.graduationYear,
+		educationLevel: d.educationLevel,
+		x: Math.random() * 900,
+		y: Math.random() * 800
+	  };
+	});
 
-    // sort them to prevent occlusion of smaller nodes.
-    myNodes.sort(function (a, b) { return b.value - a.value; });
+	// sort them to prevent occlusion of smaller nodes.
+	myNodes.sort(function (a, b) { return b.value - a.value; });
 
-    return myNodes;
+	return myNodes;
   }
 
-  /*
-   * Main entry point to the bubble chart. This function is returned
-   * by the parent closure. It prepares the rawData for visualization
-   * and adds an svg element to the provided selector and starts the
-   * visualization creation process.
-   *
-   * selector is expected to be a DOM element or CSS selector that
-   * points to the parent element of the bubble chart. Inside this
-   * element, the code will add the SVG continer for the visualization.
-   *
-   * rawData is expected to be an array of data objects as provided by
-   * a d3 loading function like d3.csv.
-   */
   var chart = function chart(selector, rawData) {
-  	//console.log(rawData);
-    var maxAmount = d3.max(rawData, function (d) { return +d.score; });
-    radiusScale.domain([0, maxAmount]);
+	var maxAmount = d3.max(rawData, function (d) { return +d.score; });
+	radiusScale.domain([0, maxAmount]);
 
-    nodes = createNodes(rawData);
-    force.nodes(nodes);
+	nodes = createNodes(rawData);
+	force.nodes(nodes);
 
-    console.log("nodes", nodes);
-    svg = d3.select(selector)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
+	svg = d3.select(selector)
+	  .append('svg')
+	  .attr('width', width)
+	  .attr('height', height);
 
-    bubbles = svg.selectAll('.bubble')
-      .data(nodes, function (d) { console.log(d.name); return d.name; });
+	bubbles = svg.selectAll('.bubble')
+	  .data(nodes, function (d) { return d.name; });
 
-    bubbles.enter().append('circle')
-      .classed('bubble', true)
-      .attr('r', 0)
-      .attr('fill', function (d) { return fillColor(d.group); })
-      .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
-      .attr('stroke-width', 2)
-      .on('mouseover', showDetail)
-      .on('mouseout', hideDetail);
+	bubbles.enter().append('circle')
+	  .classed('bubble', true)
+	  .attr('r', 0)
+	  .attr('fill', function (d) { return fillColor(d.group); })
+	  .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
+	  .attr('stroke-width', 2)
+	  .on('mouseover', showDetail)
+	  .on('mouseout', hideDetail);
 
-    bubbles.transition()
-      .duration(2000)
-      .attr('r', function (d) { return d.radius; });
+	bubbles.transition()
+	  .duration(2000)
+	  .attr('r', function (d) { return d.radius; });
 
-    groupBubbles();
+	groupBubbles();
   };
 
   function groupBubbles() {
 
-    force.on('tick', function (e) {
-      bubbles.each(moveToCenter(e.alpha))
-        .attr('cx', function (d) { return d.x; })
-        .attr('cy', function (d) { return d.y; });
-    });
+	force.on('tick', function (e) {
+	  bubbles.each(moveToCenter(e.alpha))
+		.attr('cx', function (d) { return d.x; })
+		.attr('cy', function (d) { return d.y; });
+	});
 
-    force.start();
+	force.start();
   }
 
   function moveToCenter(alpha) {
-    return function (d) {
-      d.x = d.x + (center.x - d.x) * damper * alpha;
-      d.y = d.y + (center.y - d.y) * damper * alpha;
-    };
+	return function (d) {
+	  d.x = d.x + (center.x - d.x) * damper * alpha;
+	  d.y = d.y + (center.y - d.y) * damper * alpha;
+	};
   }
 
-  function splitBubbles() {
-    force.on('tick', function (e) {
-      bubbles.each(moveToYears(e.alpha))
-        .attr('cx', function (d) { return d.x; })
-        .attr('cy', function (d) { return d.y; });
-    });
+  function splitBubbles(filters) {
+	force.on('tick', function (e) {
+	  bubbles.each(moveToYears(e.alpha, filters))
+		.attr('cx', function (d) { return d.x; })
+		.attr('cy', function (d) { return d.y; });
+	});
 
-    force.start();
+	force.start();
   }
 
-  function moveToYears(alpha) {
-    return function (d) {
-      var target = yearCenters[d.year];
-      d.x = d.x + (target.x - d.x) * damper * alpha * 1.1;
-      d.y = d.y + (target.y - d.y) * damper * alpha * 1.1;
-    };
+  function moveToYears(alpha, filters) {
+	return function (d) {
+		//set center variable
+		var uniCombo = "";
+		for(j = 0; j < filters.length; j++){
+			for(i = 0; i < filters[j][1].length; i++){
+				uniCombo += i.toString();
+			}
+		}
+
+
+
+
+
+
+	  var target = centers[d.year];
+	  d.x = d.x + (target.x - d.x) * damper * alpha * 1.1;
+	  d.y = d.y + (target.y - d.y) * damper * alpha * 1.1;
+	};
   }
 
   function showDetail(d) {
-    d3.select(this).attr('stroke', 'black');
+	d3.select(this).attr('stroke', 'black');
 
-    console.log(d);
 
-    var content = '<span class="name">Title: </span><span class="value">' +
-                  d.name +
-                  '</span><br/>' +
-                  '<span class="name">Amount: </span><span class="value">$' +
-                  addCommas(d.value) +
-                  '</span><br/>' +
-                  '<span class="name">Year: </span><span class="value">' +
-                  d.year +
-                  '</span>';
 
-    //tooltip.showTooltip(content, d3.event);
+	var content = '<div class="content">';
+	content += '<div class="row"><div class="col-md-12"><span class="name">'+d.name+', </span>';
+	content += '<span class="educationLevel">'+d.educationLevel+'</span></div></div>';
+	content += '<div class="row"><div class="col-md-12"><span class="school">'+d.school+'</span></div></div>';
+	content += '<div class="row">';
+	content += '<div class="col-md-12"><span class="major text-left">'+d.major+', </span>';
+	content += '<span class="graduationYear text-right">'+d.graduationYear+'</span></div></div>';
+	content += '<div class="row"><div class="col-md-12"<span class="hackathons">'+d.hackathons+'</span></div></div>';
+	//content += '</div>';
+
+	tooltip.showTooltip(content, d3.event);
   }
 
   function hideDetail(d) {
-    d3.select(this)
-      .attr('stroke', d3.rgb(fillColor(d.group)).darker());
+	d3.select(this)
+	  .attr('stroke', d3.rgb(fillColor(d.group)).darker());
 
-    //tooltip.hideTooltip();
+	tooltip.hideTooltip();
   }
-  
-  // return the chart function from closure.
   return chart;
 }
 
 
 var myBubbleChart = bubbleChart();
 
-/*
- * Function called once data is loaded from CSV.
- * Calls bubble chart function to display inside #vis div.
- */
-function display(data) {
-  myBubbleChart('#vis', data);
+
+function contains(arr, key, val) {
+    for (var i = 0; i < arr.length; i++) {
+        if(arr[i][key] === val) return true;
+    }
+    return false;
+}
+
+function display(data, filters) {
+  if(filters){
+  	var filteredData = [];
+  	console.log(filters);
+  	for(var key in data){
+  		if(data.hasOwnProperty(key)) {
+  			for(filter in filters){
+  				if(filters.hasOwnProperty(filter)){
+  					if(filters[filter].includes(data[key][filter])){
+  						filteredData.push(data[key]);
+  					}
+  				}
+  			}
+	    } 
+  	}
+  	//console.log(filteredData);
+  	myBubbleChart('#vis',filteredData);
+  }else{
+	myBubbleChart('#vis', data);
+  }
 }
 
 function addCommas(nStr) {
@@ -325,15 +362,87 @@ function addCommas(nStr) {
   var x2 = x.length > 1 ? '.' + x[1] : '';
   var rgx = /(\d+)(\d{3})/;
   while (rgx.test(x1)) {
-    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	x1 = x1.replace(rgx, '$1' + ',' + '$2');
   }
 
   return x1 + x2;
 }
 
-// Load the data.
+function floatingTooltip(tooltipId, width) {
+  // Local variable to hold tooltip div for
+  // manipulation in other functions.
+  var tt = d3.select('body')
+    .append('div')
+    .attr('class', 'tooltip')
+    .attr('id', tooltipId)
+    .style('pointer-events', 'none');
 
-//d3.csv('data/gates_money.csv', display);
+  // Set a width if it is provided.
+  if (width) {
+    tt.style('width', width);
+  }
 
-// setup the buttons.
-//setupButtons();
+  // Initially it is hidden.
+  hideTooltip();
+
+  /*
+   * Display tooltip with provided content.
+   *
+   * content is expected to be HTML string.
+   *
+   * event is d3.event for positioning.
+   */
+  function showTooltip(content, event) {
+    tt.style('opacity', 1.0)
+      .html(content);
+
+    updatePosition(event);
+  }
+
+  /*
+   * Hide the tooltip div.
+   */
+  function hideTooltip() {
+    tt.style('opacity', 0.0);
+  }
+
+  /*
+   * Figure out where to place the tooltip
+   * based on d3 mouse event.
+   */
+  function updatePosition(event) {
+    var xOffset = 20;
+    var yOffset = 10;
+
+    var ttw = tt.style('width');
+    var tth = tt.style('height');
+
+    var wscrY = window.scrollY;
+    var wscrX = window.scrollX;
+
+    var curX = (document.all) ? event.clientX + wscrX : event.pageX;
+    var curY = (document.all) ? event.clientY + wscrY : event.pageY;
+    var ttleft = ((curX - wscrX + xOffset * 2 + ttw) > window.innerWidth) ?
+                 curX - ttw - xOffset * 2 : curX + xOffset;
+
+    if (ttleft < wscrX + xOffset) {
+      ttleft = wscrX + xOffset;
+    }
+
+    var tttop = ((curY - wscrY + yOffset * 2 + tth) > window.innerHeight) ?
+                curY - tth - yOffset * 2 : curY + yOffset;
+
+    if (tttop < wscrY + yOffset) {
+      tttop = curY + yOffset;
+    }
+
+    tt.style({ top: tttop + 'px', left: ttleft + 'px' });
+  }
+
+  return {
+    showTooltip: showTooltip,
+    hideTooltip: hideTooltip,
+    updatePosition: updatePosition
+  };
+}
+
